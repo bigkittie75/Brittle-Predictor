484ff133c633dd422680242c393fc5bea05bdb6b
aric <bigkittie75@gmail.com>
2022-03-27 10:25:33 -0700

    Messiness. Changed disc method, added down sampling.

--- r code/JRip Caret.R
+++ r code/JRip Caret.R
@@ -1,3 +1,8 @@
+# disc method matters, currently chimerge works best
+# minimal, but noticable performance boost by removing Region
+# down sampling improves results due to imbalance of classifiers, need to test other sampling options in the future
+#
+
 pacman::p_load(  # Use p_load function from pacman
   arulesCBA,        # for disc
   RWeka,         # For Ripper Rule Classification
@@ -7,7 +12,8 @@ pacman::p_load(  # Use p_load function from pacman
   rio,           # Import/export data
   tidyverse,     # So many reasons
   #plyr,          
-  doParallel     
+  doParallel,
+  tictoc
 )
 
 # use if training on split data from same sample
@@ -18,6 +24,9 @@ pacman::p_load(  # Use p_load function from pacman
 
 trn <- import("data/lcpaper.rds")
 tst <- import("data/csudhsample.rds")
+
+trn <- select(trn, -Region)
+tst <- select(tst, -Region)
 
 ####### Grouping and disc
 
@@ -35,7 +44,7 @@ df <-
   discretizeDF.supervised(  # Function from `arulesCBA`
     y ~ .,                  # Brittle based on rest
     data = df,              # Data source
-    method = "mdlp"         # Algorithm to use
+    method = "chimerge"         # Algorithm to use chi2, chimerge, mdlp
   )
 
 # Splitting on group column for training and testing sample
@@ -50,7 +59,7 @@ rm(df)
 
 ## init parallel
 numCores <- detectCores()
-cl <- makePSOCKcluster(8) #replace # with results from detectCores()
+cl <- makePSOCKcluster(16) #replace # with results from detectCores()
 registerDoParallel(cl) #start parallel
 ####################
 
@@ -60,17 +69,18 @@ set.seed(420)
 seeds <- vector(mode = "list", length = 11)
 
 #(4 is the number of tuning parameter)
-for(i in 1:10) seeds[[i]]<- sample.int(n=1000, 75)
+for(i in 1:10) seeds[[i]]<- sample.int(n=100000, 25000)
 
 #for the last model
-seeds[[11]]<-sample.int(1000, 1)
+seeds[[11]]<-sample.int(100000, 1)
 
-fitControl <- trainControl(method = "repeatedcv",
+fitControl <- trainControl(method = "boot",
                            number = 10,   ## 10-fold CV
-                           repeats = 1,   ## Repeated n times
-                           seeds = seeds,
-                           classProbs = TRUE,
-                           summaryFunction = twoClassSummary)  
+                           #repeats = 3,   ## Repeated n times
+                           #seeds = seeds, ##uncomment for JRIP2 remove ")" and add comma for JRIP2
+                           #classProbs = TRUE, ##uncomment for JRIP2
+                           #summaryFunction = twoClassSummary,  ##uncomment for JRIP2
+                           sampling = "down") 
 
 JRipFit1 <- train(y ~ ., data = trn, 
                  method = "JRip", 
@@ -78,33 +88,39 @@ JRipFit1 <- train(y ~ ., data = trn,
 
 JRipFit1
 
-JRipGrid <- expand.grid(NumOpt = seq(1, 5, 1),
-                       NumFolds = seq(2, 4, 1),
-                       MinWeights = seq(1, 5, 1))
+JRipGrid <- expand.grid(NumOpt = seq(1, 10, 1),
+                       NumFolds = 10,
+                       MinWeights = 10)
 
+tic("25000 models time:")
 JRipFit2 <- train(y ~ ., data = trn, 
                  method = "JRip", 
                  trControl = fitControl, 
                  #verbose = FALSE, 
                  tuneGrid = JRipGrid,
-                 metric = "ROC")
+                 metric = "Sens")
+toc()
+#25000 models took 4.65 hours
+#1 model took 19.68 seconds
+#10 models took 23.33 seconds
 
 JRipFit2
 
-whichTwoPct <- tolerance(JRipFit2$results, metric = "Sens", 
-                         tol = 2, maximize = TRUE) 
+#JRipFit2 %>% saveRDS("data/JRipFit(ROC).rds")
 
-JRipFit2$results[whichTwoPct,1:6]
-JRipFit2$finalModel
+#whichTwoPct <- tolerance(JRipFit2$results, metric = "ROC", 
+#                         tol = 2, maximize = TRUE) 
+
+#JRipFit2$results[whichTwoPct,1:6]
+#JRipFit2$finalModel
 
 # Check accuracy of the model on the training data
 confusionMatrix(      # Create a confusion matrix
   reference = tst$y,  # True values
   predict(            # Predicted values
-    #JRipFit1,              # Based on the training model 1
-    JRipFit2$finalModel,              # Based on the training model 2
+    JRipFit1,              # Based on the training model 1
+    #JRipFit2$finalModel,              # Based on the training model 2
     newdata = tst,    # Use the training data
-    type = 'class'
   )
 )
 
